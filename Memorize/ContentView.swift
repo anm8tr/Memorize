e0c1b3b8b6f0d7373e0abb53db759ebcb035747c
anm8tr <threeduser@hotmail.com>
2023-12-29 21:16:14 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Richard Garrison on 12/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(lineWidth: 2)
+            Text("👻").font(.largeTitle)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Richard Garrison on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
